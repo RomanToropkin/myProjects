a0fff5189ad1c4265c04f528f0ebf1d5e32bc53f
Toropkin Roman <good.romka@yandex.ru>
2018-08-03 13:09:46 +0300

    Голый UI

---
+++ app/src/main/java/com/example/roman/mynotes/Fragments/NotesListFragment.java
@@ -0,0 +1,32 @@
+package com.example.roman.mynotes.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.roman.mynotes.R;
+
+/**
+ * Created by roman on 03.08.2018.
+ */
+
+public class NotesListFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.notes_list_layout, container, false);
+
+        return view;
+    }
+
+}
